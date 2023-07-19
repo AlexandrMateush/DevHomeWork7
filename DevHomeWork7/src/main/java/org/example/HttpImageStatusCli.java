657d445ff7af4849e283692e9b8b1498ec1a0164
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-07-19 19:40:48 +0300

    Add files via upload

---
+++ DevHomeWork7/src/main/java/org/example/HttpImageStatusCli.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HttpImageStatusCli {
+    public void askStatus() {
+        System.out.println("Enter HTTP status code:");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String input = reader.readLine();
+            try {
+                int code = Integer.parseInt(input);
+                HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
+                downloader.downloadStatusImage(code);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+            } catch (IOException e) {
+                System.out.println("There is no image for HTTP status " + input);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
