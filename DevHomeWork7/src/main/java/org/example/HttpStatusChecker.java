5c5ddf4d12f34ecc9b1f977cea684d051c530263
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-07-20 17:22:10 +0300

    Add files via upload

--- DevHomeWork7/src/main/java/org/example/HttpStatusChecker.java
+++ DevHomeWork7/src/main/java/org/example/HttpStatusChecker.java
@@ -5,15 +5,20 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpStatusChecker {
-    public String getStatusImage(int code) throws IOException {
+    public String getStatusImage(int code) {
         String url = "https://http.cat/" + code + ".jpg";
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestMethod("HEAD");
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            return url;
-        } else {
-            throw new RuntimeException("There is no image for HTTP status " + code);
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                return url;
+            } else {
+                System.out.println("There is no image for HTTP status " + code);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while checking the HTTP status: " + e.getMessage());
         }
+        return "";
     }
 }
